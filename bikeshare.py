a662f314294801ec61f41cec281d5ec0d3fee8b1
Rohit Deshmukh <rohdeshmukh@gmail.com>
2020-06-06 01:53:08 +0530

    refactor:refactoring production code
    Redundant print statement is deleted and addition of that statement in input message.

--- bikeshare.py
+++ bikeshare.py
@@ -15,9 +15,9 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = input("Please enter city name \nNote that data of these cities are available(chicago, new york city, washington) :").lower()
+    city = input("Hello! Let\'s explore some US bikeshare data! \nPlease enter city name \nNote that data of these cities are available(chicago, new york city, washington) :").lower()
     
    
 
